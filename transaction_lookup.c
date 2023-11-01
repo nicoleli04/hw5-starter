5ac3f7f114b5883b70f8e1b37115f71603c63f9d
MKekkar <mkekkar@ucsd.edu>
2023-11-01 02:59:01 -0700

    add message about usage format

--- transaction_lookup.c
+++ transaction_lookup.c
@@ -9,12 +9,16 @@
 #define DEFAULT_TABLE_SIZE 1873
 #define QUERY_SUCCESS_FORMAT "found sale id=%s, purchased_item=%s, cost=%.5f\n" 
 #define QUERY_FAILURE_FORMAT "could not find sale with id=%s\n"
+#define USAGE_STRING "%s [-s] [-t table_size] <filename>"
 
 int main(int argc, char **argv) {
 	int s_flag;
 	unsigned long table_size;
 	// TODO: parse opts with getopt and initialize these variables 
 	// (see strtoul for parsing unsigned long)
+	// handle any bad inputs (missing filename or invalid option flag
+	// by printing out the USAGE_STRING to stderr and exiting:
+	// fprintf(stderr, USAGE_STRING, argv[0]); // you can use this line
 
 	
 	// TODO: initialze htable
